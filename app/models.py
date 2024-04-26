cc66b8041aba416de829ae66a6d5796d4d6eb9e5
JeanKim7 <jean.g.km@gmail.com>
2024-04-25 22:44:53 -0700

    return user in login

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class User(db.Model):
         self.token=secrets.token_hex(16)
         self.token_expiration = now + timedelta(hours=1)
         self.save()
-        return {"token": self.token, "tokenExpiration":self.token_expiration}
+        return {"token": self.token, "tokenExpiration":self.token_expiration, "currentUser": self}
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
